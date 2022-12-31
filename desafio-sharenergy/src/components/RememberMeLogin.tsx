dd6be6b511b7c842ae8df222feca776cb8725b71
pedroygor <pedroygor2010@hotmail.com>
2022-12-31 18:47:59 -0300

    feat: reset project

--- desafio-sharenergy/src/components/RememberMeLogin.tsx
+++
@@ -1,8 +0,0 @@
-export function RememberMeLogin() {
-  return (
-    <label htmlFor="rememberMe">
-      <span>Lembrar de mim</span>
-      <input type="checkbox" name="rememberMe" id="rememberMe" />
-    </label>
-  )
-}

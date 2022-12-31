3380df78a17757a80dd6cf60cdf9094b9d8a615e
pedroygor <pedroygor2010@hotmail.com>
2022-12-31 18:48:46 -0300

    feat: add React Router Dom

--- desafio-sharenergy/src/main.tsx
+++ desafio-sharenergy/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Login } from './pages/Login'
+import UserGenerator from './pages/UserGenerator'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/generator' element={<UserGenerator />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
